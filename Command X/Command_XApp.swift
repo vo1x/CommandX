652f2ffcb3dd64d4207a293134829f14f8e47b16
vo1x <ak2061@icloud.com>
2026-02-05 18:20:57 +0545

    Initial Commit

---
+++ Command X/Command_XApp.swift
@@ -0,0 +1,17 @@
+//
+//  Command_XApp.swift
+//  Command X
+//
+//  Created by Aayush Kandel on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Command_XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Command XTests/Command_XTests.swift
@@ -0,0 +1,17 @@
+//
+//  Command_XTests.swift
+//  Command XTests
+//
+//  Created by Aayush Kandel on 05/02/2026.
+//
+
+import Testing
+@testable import Command_X
+
+struct Command_XTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
